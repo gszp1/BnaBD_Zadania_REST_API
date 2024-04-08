669e74e069d4641596e3a3578686355d2a6bc840
gszp1 <grzybekmateusz@poczta.fm>
2024-04-08 17:20:50 +0200

    Adds EmployeeService Service.

---
+++ src/main/java/org/example/zadania_rest_api/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package org.example.zadania_rest_api.service;
+
+import org.example.zadania_rest_api.model.Employee;
+import org.example.zadania_rest_api.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Optional<Employee> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
