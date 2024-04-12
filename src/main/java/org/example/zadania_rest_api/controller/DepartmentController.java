6a9d60dfa9fd6fc96b46909b546529abb7653cb7
gszp1 <grzybekmateusz@poczta.fm>
2024-04-12 17:46:45 +0200

    Adds DepartmentController controller.

---
+++ src/main/java/org/example/zadania_rest_api/controller/DepartmentController.java
@@ -0,0 +1,18 @@
+package org.example.zadania_rest_api.controller;
+
+import org.example.zadania_rest_api.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+}
